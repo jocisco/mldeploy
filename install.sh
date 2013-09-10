6435591b0635cce80264a024d11bfb1223fe21b6
jocisco <jogarzon@cisco.com>
2013-09-10 10:36:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-./mldeploy
+date >> ~/vagrant_provisioned_at                                                                                                                                                
+sudo yum install git -y
+echo Dowloading deployment scripts
+[[ -d mldeploy ]] || git clone https://github.com/jocisco/mldeploy.git
+cd mldeploy
+git pull
+bash mldeploy
